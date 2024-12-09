b08f5a73b2e63c98d1eea47c8c1574b07bcfc063
babakohei0726 <20148@hakodate.kosen-ac.jp>
2024-12-09 14:30:25 +0900

    a

--- game12/classroom.java
+++ game12/classroom.java
@@ -16,7 +16,7 @@ public class classroom extends World
     public classroom()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        getBackground().scale( 100, 100 );
+        super(600, 500, 1); 
+        getBackground().scale( getWidth(), getHeight() );
     }
 }
